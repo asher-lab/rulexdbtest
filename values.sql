dedcafceaa39b65718376a93aa286b0fc1e27263
Asher  Manangan <51806917+asher-lab@users.noreply.github.com>
2023-11-30 15:21:27 +0800

    Update values.sql

--- values.sql
+++ values.sql
@@ -2,4 +2,4 @@ TRUNCATE TABLE EMEA;
 
 INSERT INTO EMEA (shipment_date, customer_name, product_name, quantity)
 VALUES
-    ('2023-09-20', 'Mhiee Vic', 'Widget A', 1100)
+    ('2023-09-20', 'Ramon', 'Widget A', 1100)
