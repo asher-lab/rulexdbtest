c542168f62900673c664af8d0e6e31858b183577
Asher  Manangan <51806917+asher-lab@users.noreply.github.com>
2023-11-30 16:57:47 +0800

    sample cicd

--- values.sql
+++ values.sql
@@ -2,4 +2,4 @@ TRUNCATE TABLE EMEA;
 
 INSERT INTO EMEA (shipment_date, customer_name, product_name, quantity)
 VALUES
-    ('2023-09-20', 'RamonS', 'Widget A', 1100)
+    ('2023-09-20', 'RamonAndBartek', 'Widget A', 1100)
